3162628bfe7133cbc5453b1be5baf41ae01471a4
snowleopard618 <snowleo1342@gmail.com>
2025-10-01 03:50:29 +0900

    change alertscheduler

--- src/components/AlertScheduler.jsx
+++ src/components/AlertScheduler.jsx
@@ -3,7 +3,8 @@ import React, { useEffect, useRef, forwardRef, useImperativeHandle } from 'react
 import { Meeting } from '../api/entities';
 import ServiceWorkerRegistration from './ServiceWorkerRegistration';
 import { storage } from '../utils/storage.js';
-import { AlertIntensity } from '../utils/notificationUtils.js';\nimport EnhancedNotificationManager from '../services/EnhancedNotificationManager';
+import { AlertIntensity } from '../utils/notificationUtils.js';
+import EnhancedNotificationManager from '../services/EnhancedNotificationManager';
 
 const AlertScheduler = forwardRef(({ onTriggerAlert, language = "en", alertsEnabled }, ref) => {
   const alertTimeoutsRef = useRef(new Map());
